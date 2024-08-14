a8b775c974ec3c1c99cab0a4984bb653f2b35100
Raifulalam <raifulalam0123@gmail.com>
2024-08-14 22:58:23 +0530

    edited

--- script.js
+++ script.js
@@ -1,44 +1,3 @@
-
-// const check = document.getElementById('check');
-
-// check.addEventListener('click', () => {
-//     // Get values from input fields and convert to lowercase
-//     let firstName = document.getElementById('firstName').value.toLowerCase().split("");
-//     let secondName = document.getElementById('secondName').value.toLowerCase().split("");
-
-//     // Save values to localStorage
-//     localStorage.setItem('first', document.getElementById('firstName').value);
-//     localStorage.setItem('second', document.getElementById('secondName').value);
-
-//     if (firstName.length > 0 && secondName.length > 0) {
-//         // Compute relationship
-//         for (let i = 0; i < firstName.length; i++) {
-//             if (secondName.indexOf(firstName[i]) !== -1) {
-//                 secondName.splice(secondName.indexOf(firstName[i]), 1);
-//             }
-//         }
-//         const difference = firstName.length + secondName.length;
-//         const flames = ['Friends', 'Lovers', 'Affectionate', 'Marriage', 'Enemies', 'Siblings'];
-//         const relation = flames[difference % 6];
-//         document.getElementById('result').innerText = `Relationship: ${relation}`;
-
-//     } else {
-//         document.getElementById('result').innerText = `Please enter both names`;
-//     }
-// });
-// window.addEventListener('load', () => {
-//     const storedFirstName = localStorage.getItem('firstName');
-//     const storedSecondName = localStorage.getItem('secondName');
-
-//     if (storedFirstName) {
-//         document.getElementById('firstName').value = storedFirstName;
-//     }
-//     if (storedSecondName) {
-//         document.getElementById('secondName').value = storedSecondName;
-//     }
-// });
-
-
 const check = document.getElementById('check');
 
 // Function to compute relationship
@@ -55,8 +14,8 @@ const computeRelationship = (a, b) => {
 
 check.addEventListener('click', () => {
     // Get values from input fields and convert to lowercase
-    let firstName = document.getElementById('firstName').value.toLowerCase();
-    let secondName = document.getElementById('secondName').value.toLowerCase();
+    let firstName = document.getElementById('firstName').value.trim().toLowerCase();
+    let secondName = document.getElementById('secondName').value.trim().toLowerCase();
     if (firstName && secondName) {
         // Compute relationship
         let a = firstName.split("");
@@ -101,7 +60,8 @@ window.addEventListener('load', () => {
         // list.innerHTML = ''; // Clear existing list
         storedData.forEach(pair => {
             const listItem = document.createElement('li');
-            listItem.textContent = `First Name: ${pair.firstName}, Second Name: ${pair.secondName}, Relation: ${pair.relation}`;
+            // listItem.textContent = `First Name: ${pair.firstName}, Second Name: ${pair.secondName}, Relation: ${pair.relation}`;
+            listItem.textContent = `Relation: ${pair.relation}`
             list.appendChild(listItem);
         });
     };
